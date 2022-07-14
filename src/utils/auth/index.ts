33ad29c89e4069f9e486e96d729e80a6ac870cbc
Chima <orlandofays3@gmail.com>
2022-07-14 20:26:55 +0100

    change yml build step

--- src/utils/auth/index.ts
+++ src/utils/auth/index.ts
@@ -1,20 +1,21 @@
-import { decodeToken, generateJWTToken, } from '..';
-import User from '../../database/entity/User';
-import moment from 'moment';
-import redis from '../../database/redis';
-import { TokenFlag } from '../../database/enum';
+/* eslint-disable space-before-function-paren */
+import { decodeToken, generateJWTToken } from '..'
+import User from '../../database/entity/User'
+import moment from 'moment'
+import redis from '../../database/redis'
+import { TokenFlag } from '../../database/enum'
 
-export async function createSession(user: User) {  
-    const token = await generateJWTToken({ userId: user.id, flag: TokenFlag.AUTH })
-    const decodedToken = await decodeToken(token);     
-           
-    const sessionKeyPrefix = `sessions:${user.id}`;
-    const sessionKey = `${sessionKeyPrefix}:${decodedToken.counter}`;
-    const expires = moment().diff(moment(decodedToken.exp), 'seconds');
-    
-    await redis.setex(sessionKey, expires, token);    
-    await redis.sadd(sessionKeyPrefix, sessionKey);    
-    await redis.expire(sessionKeyPrefix, expires);    
+export async function createSession(user: User) {
+  const token = await generateJWTToken({ userId: user.id, flag: TokenFlag.AUTH })
+  const decodedToken = await decodeToken(token)
 
-    return token;
+  const sessionKeyPrefix = `sessions:${user.id}`
+  const sessionKey = `${sessionKeyPrefix}:${decodedToken.counter}`
+  const expires = moment().diff(moment(decodedToken.exp), 'seconds')
+
+  await redis.setex(sessionKey, expires, token)
+  await redis.sadd(sessionKeyPrefix, sessionKey)
+  await redis.expire(sessionKeyPrefix, expires)
+
+  return token
 }
