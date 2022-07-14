78d71721f84c8d9ca7a3bb89ced8d373e42b587f
Chima <orlandofays3@gmail.com>
2022-07-14 19:45:15 +0100

    add eslint

--- src/services/user/getUser.ts
+++ src/services/user/getUser.ts
@@ -1,12 +1,11 @@
-import { wrapServiceAction } from "..";
-import UserRepo from "../../database/repositories/UserRepository";
-import { GetUserRequest } from "../types/users";
-
+import { wrapServiceAction } from '..'
+import UserRepo from '../../database/repositories/UserRepository'
+import { GetUserRequest } from '../types/users'
 
 export default wrapServiceAction({
-    schema: GetUserRequest,
-    handler: async (params: GetUserRequest) => {
-        const users = await UserRepo.getUserById(params.userId);
-        return users;
-    }
+  schema: GetUserRequest,
+  handler: async (params: GetUserRequest) => {
+    const users = await UserRepo.getUserById(params.userId)
+    return users
+  }
 })
